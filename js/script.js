cbd62e2939dfd098123bc7ae845d931255b64ba9
ArbeyMaya <vilmarbey@gmail.com>
2023-07-24 22:15:21 -0500

    Se actualizo el script

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//console.log("Analisis y Desarrollo de Software");
+
+//alert("Analisis y Desarrollo de Software");
+
+//para comentar en una linea de codigo 
+
+/* este tipo de comentario
+se trabaja en diferentes 
+lineas de codigo*/
+
+//document.write("Analisis y Desarrollo de Software");
+
+//declarar bariables var
+
+//var edad=33;
+//const nom="Arbey";
+//let num=2.5;
+//booleano='ADSO';
+
+/* unir dos cadenas de texto
+const nom="Arbey"
+const ape='Maya'
+alert("mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/* operadores aritmeticos +,-,*,/
+const num1=5
+const num2=3
+const div = num1/num2
+alert("la division es: "+div) */
+
+let nom=prompt("escriba el nombre")
+let ape=prompt("escriba su apellido")
+let edad=prompt("escriba su edad")
+let peso=prompt("escriba su peso")
+let direccion=prompt("escriba su direccion")
+alert(" mi nombre es: "+nom+ " Mi apellido es: "+ape+ " Mi edad es: "+edad+ " Mi peso es: "+peso+ " Mi dirección es: "+direccion )
+
+/*mostrar una alerta con los nombres y apellidos   
+completos, edad, peso y direccion*/
+
+//console.log(typeof booleano); typeof es para definir el tipo de dato
+
+//console.log("mi edad es: "+edad);
+//alert("mi edad es: "+edad);
+//document.write("mi edad es: "+edad)
+//simbolo "+" sirve para concatenar
+
+//const edad2=22;
+
+//let edad3=33;
